a6a886525877ea9189dc66e057493e211d27cc02
Dinika <dinikasaxenas@gmail.com>
2019-04-10 02:42:27 +0530

    fetch and post todo items to db
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Todo = () => {
   const [todoName, onTodoNameChange] = useState('');
   const [todoList, onTodoAdded] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get('https://todo-b8ab2.firebaseio.com/todos.json')
+      .then(response => {
+        console.log(response);
+        const todoData = response.data;
+        const todos = [];
+        for (let key in todoData) {
+          todos.push(todoData[key].name);
+        }
+        onTodoAdded(todos);
+        console.log(todoList);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
   const inputChangedHandler = event => {
     onTodoNameChange(event.target.value);
   };
 
   const todoAddedHandler = () => {
     onTodoAdded(todoList.concat(todoName));
+    axios
+      .post('https://todo-b8ab2.firebaseio.com/todos.json', {
+        name: todoName
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
+
   return (
     <React.Fragment>
       <input
