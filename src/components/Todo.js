095664850368e854270dfc5df0f89e3f0c7a932f
Dinika <dinikasaxenas@gmail.com>
2019-04-17 18:30:47 +0530

    optimize rendering of todoList
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect, useReducer, useRef } from 'react';
+import React, { useState, useEffect, useReducer, useRef, useMemo } from 'react';
 import List from './List';
 import axios from 'axios';
 
 const Todo = () => {
   const [todoList, dispatch] = useReducer(todoListReducer, []);
   const todoNameRef = useRef(null);
+  const [isInputValid, setIsInputValid] = useState(false);
 
   function todoListReducer(state, action) {
     switch (action.type) {
@@ -32,7 +33,6 @@ const Todo = () => {
       })
       .catch(error => {
         console.log(error);
-        dispatch({ type: 'SET', payload: ['Yeah', 'Yo', 'Yep'] });
       });
   }, []);
 
@@ -55,11 +55,27 @@ const Todo = () => {
     dispatch({ type: 'SUBSTRACT', payload: index });
   };
 
+  const checkInputValidation = event => {
+    const input = event.target.value;
+    const isValid = input.trim() === '' ? false : true;
+    setIsInputValid(isValid);
+  };
+
+  const renderTodoList = () => (
+    <List onTodoClick={onTodoClick} todoList={todoList} />
+  );
+
   return (
     <React.Fragment>
-      <input type="text" placeholder="Todo" ref={todoNameRef} />
+      <input
+        type="text"
+        placeholder="Todo"
+        ref={todoNameRef}
+        onChange={checkInputValidation}
+        style={{ background: isInputValid ? 'transparent' : 'red' }}
+      />
       <button onClick={todoAddedHandler}>Add Todo</button>
-      <List onTodoClick={onTodoClick} todoList={todoList} />
+      {useMemo(renderTodoList, [todoList])}
     </React.Fragment>
   );
 };
